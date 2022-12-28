df90850ceade7b29aedc88c95d185423a2d568da
Amin Bakhshi <bakhshiamin85@gmail.com>
2022-12-28 12:15:34 +0100

    feat(solution): chunke an array

--- problems/chunk/index.js
+++ problems/chunk/index.js
@@ -8,6 +8,28 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  const chunked = [];
+  // 1
+  //   array.map((item) => {
+  //     const last_chunk = chunked[chunked.length - 1];
+
+  //     if (!last_chunk || last_chunk.length === size) {
+  //       chunked.push([item]);
+  //     } else {
+  //       last_chunk.push(item);
+  //     }
+  //   });
+  //   2
+  let index = 0;
+
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size));
+
+    index += size;
+  }
+
+  return chunked;
+}
 
 module.exports = chunk;
