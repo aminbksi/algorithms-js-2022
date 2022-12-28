ae34157452bd9c062f869cf5c297169802d70517
Amin Bakhshi <bakhshiamin85@gmail.com>
2022-12-28 17:46:20 +0100

    feat(solution): first anagarams solution

--- problems/anagrams/index.js
+++ problems/anagrams/index.js
@@ -8,6 +8,31 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  const strAMapped = mapChars(stringA);
+  const strBMapped = mapChars(stringB);
+
+  if (Object.keys(strAMapped).length !== Object.keys(strBMapped).length) {
+    return false;
+  }
+
+  for (let char in strAMapped) {
+    if (strAMapped[char] !== strBMapped[char]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function mapChars(str) {
+  const charMap = {};
+
+  for (let char of str.replace(/[^\w]/g, "").toLowerCase()) {
+    charMap[char] = charMap[char] ? charMap[char] + 1 : 1;
+  }
+
+  return charMap;
+}
 
 module.exports = anagrams;
