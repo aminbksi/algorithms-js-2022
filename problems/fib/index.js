10702cf977df6176c42afdf04d7f12b9aa4f260f
Amin Bakhshi <bakhshiamin85@gmail.com>
2022-12-29 20:58:11 +0100

    feat(solution): memoizing fib recursion

--- problems/fib/index.js
+++ problems/fib/index.js
@@ -18,12 +18,31 @@
 //   return results[n];
 // }
 // recursion
-function fib(n) {
+
+function slowFib(n) {
   if (n < 2) {
     return n;
   }
 
   return fib(n - 1) + fib(n - 2);
 }
+
+//  memoizing
+
+function memoize(fn) {
+  const cache = {};
+  return function (...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  };
+}
+
+const fib = memoize(slowFib);
 
 module.exports = fib;
