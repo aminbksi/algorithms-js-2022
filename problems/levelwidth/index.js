1f5479025c8099469ad69de53a24ba2a6f5c7324
Amin Bakhshi <bakhshiamin85@gmail.com>
2023-01-02 14:45:53 +0100

    feat(solution): width level of tree

--- problems/levelwidth/index.js
+++ problems/levelwidth/index.js
@@ -11,6 +11,29 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  const counter = [];
+  const arr = [root, "s"];
+
+  let count = 0;
+
+  while (arr.length > 1) {
+    const node = arr.shift();
+
+    if (node === "s") {
+      counter.push(count);
+      arr.push("s");
+      count = 0;
+    } else {
+      arr.push(...node.children);
+      count++;
+      if (arr.length === 1) {
+        counter.push(count);
+      }
+    }
+  }
+
+  return counter;
+}
 
 module.exports = levelWidth;
